097333c23f0a066337d81e615dc7abcf45e3331a
marabend <143610328+marabend@users.noreply.github.com>
2024-05-29 21:30:07 +0200

    Create snippet_0001.asm

---
+++ snippet_0001.asm
@@ -0,0 +1,12 @@
+includelib kernel32.lib ; Import a standard Windows library
+ExitProcess proto		; Define an imported library function. 
+
+.data					; Start of the data section
+						; <- Variable declarations go here.
+.code					; Start of the code section
+main proc				; Program entry procedure.
+						; <- Assembly instructions go here.
+call ExitProcess		; Execute the imported library function
+
+main endp				; End of the main procedure
+end						; End of the Assembly program
